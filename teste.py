2e70e4734526f5879756d759a17b55529a7930bc
CarreraAlberto <albertomcarrera@hotmail.com>
2021-05-12 17:29:26 -0300

    teste de mesa

--- teste.py
+++ teste.py
@@ -1,1 +1,29 @@
-print('Hello world!')
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((700, 500))
+pygame.display.set_caption('Arco e flecha:)')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((0, 33, 115))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
