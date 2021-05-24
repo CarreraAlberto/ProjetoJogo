402fc467a89e2d94b0eaced4ea3308a0ffcaccdc
Gabriel Barbosa <gabrielbcb@al.insper.edu.br>
2021-05-24 18:08:34 -0300

    arqueiro posicao certa

--- primeiras_configurações.py
+++ primeiras_configurações.py
@@ -6,11 +6,11 @@ window = pygame.display.set_mode((1500, 500))
 pygame.display.set_caption('Tiro ao alvo')
  
 game = True
-
+ 
 image_bloco2 = pygame.image.load('imagens/bloco2.png').convert()
 image_arqueiro = pygame.image.load('imagens/arqueiro.JPG').convert()
 image_arqueiro = pygame.transform.scale(image_arqueiro, (188, 274))
-
+ 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,7 +21,7 @@ while game:
     while distancia_x < 1501:
         window.blit(image_bloco2, (distancia_x, 420))
         distancia_x+=250
-    window.blit(image_arqueiro, (0, 0))
+    window.blit(image_arqueiro, (0, 145))
     pygame.display.update()
  
 pygame.quit()
