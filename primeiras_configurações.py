98903b4cd7b38bffe3b84889ef5984da07607cc6
CarreraAlberto <albertomcarrera@hotmail.com>
2021-05-24 18:03:24 -0300

    com arqueiro

--- primeiras_configurações.py
+++ primeiras_configurações.py
@@ -7,7 +7,7 @@ pygame.display.set_caption('Tiro ao alvo')
  
 game = True
 
-image_bloco = pygame.image.load('imagens/bloco.png').convert()
+image_bloco2 = pygame.image.load('imagens/bloco2.png').convert()
 image_arqueiro = pygame.image.load('imagens/arqueiro.JPG').convert()
 image_arqueiro = pygame.transform.scale(image_arqueiro, (188, 274))
 
@@ -19,7 +19,7 @@ while game:
     window.fill((255, 255, 255))
     distancia_x = 0
     while distancia_x < 1501:
-        window.blit(image_bloco, (distancia_x, 420))
+        window.blit(image_bloco2, (distancia_x, 420))
         distancia_x+=250
     window.blit(image_arqueiro, (0, 0))
     pygame.display.update()
