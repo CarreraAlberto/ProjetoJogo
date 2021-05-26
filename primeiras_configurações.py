4061019d0b803a8840e84137fed17907d127aa0d
CarreraAlberto <albertomcarrera@hotmail.com>
2021-05-26 16:10:59 -0300

    o alvo

--- primeiras_configurações.py
+++ primeiras_configurações.py
@@ -10,7 +10,9 @@ game = True
 image_bloco2 = pygame.image.load('imagens/bloco2.png').convert()
 image_arqueiro = pygame.image.load('imagens/arqueiro.JPG').convert()
 image_arqueiro = pygame.transform.scale(image_arqueiro, (188, 274))
- 
+image_alvo = pygame.image.load('imagens/Covid_alvo.png').convert()
+image_alvo = pygame.transform.scale(image_alvo, (115, 115))
+
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,6 +24,7 @@ while game:
         window.blit(image_bloco2, (distancia_x, 420))
         distancia_x+=250
     window.blit(image_arqueiro, (0, 145))
+    window.blit(image_alvo, (1000, 145))
     pygame.display.update()
  
 pygame.quit()
