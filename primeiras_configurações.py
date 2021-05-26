a97b2ad5703eb1226baf7ac11feca9666e5653fb
CarreraAlberto <albertomcarrera@hotmail.com>
2021-05-26 16:18:04 -0300

    explicações

--- primeiras_configurações.py
+++ primeiras_configurações.py
@@ -1,23 +1,32 @@
 import pygame
  
 pygame.init()
- 
+
+# ----- Gera tela principal 
 window = pygame.display.set_mode((1500, 500))
 pygame.display.set_caption('Tiro ao alvo')
- 
-game = True
- 
+
+# ----- Inicia assets 
 image_bloco2 = pygame.image.load('imagens/bloco2.png').convert()
 image_arqueiro = pygame.image.load('imagens/arqueiro.JPG').convert()
 image_arqueiro = pygame.transform.scale(image_arqueiro, (188, 274))
 image_alvo = pygame.image.load('imagens/Covid_alvo.png').convert()
 image_alvo = pygame.transform.scale(image_alvo, (115, 115))
 
+# ----- Inicia estruturas de dados 
+game = True
+
+# ===== Loop principal =====
 while game:
+    # ----- Trata eventos
     for event in pygame.event.get():
+        # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
- 
+    
+    # ----- Atualiza estado do jogo
+
+    # ----- Gera saídas
     window.fill((255, 255, 255))
     distancia_x = 0
     while distancia_x < 1501:
@@ -26,5 +35,6 @@ while game:
     window.blit(image_arqueiro, (0, 145))
     window.blit(image_alvo, (1000, 145))
     pygame.display.update()
- 
+
+# ===== Finalização =====
 pygame.quit()
