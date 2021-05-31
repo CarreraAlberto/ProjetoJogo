69d4e065b9978321a89a2ab843a085b22a223a54
CarreraAlberto <albertomcarrera@hotmail.com>
2021-05-31 17:32:24 -0300

    agora atira

--- Mecanicas.py
+++ Mecanicas.py
@@ -11,6 +11,7 @@ larguraw = 1500
 alturaw = 750
 largurac = 100
 alturac = 100
+xlinha = 200
 window = pygame.display.set_mode((larguraw, alturaw))
 pygame.display.set_caption('Libere a Badernahhh')
  
@@ -21,12 +22,13 @@ image_arqueiro = pygame.transform.scale(image_arqueiro, (188, 274))
 image_alvo = pygame.image.load('imagens/Covid_alvo.png').convert_alpha()
 image_alvo = pygame.transform.scale(image_alvo, (largurac, alturac))
 image_linha = pygame.image.load('imagens/linha.png').convert_alpha()
-
+image_vacina = pygame.image.load('imagens/vacina.png').convert_alpha()
+image_vacina = pygame.transform.scale(image_vacina, (150, 75))
 
 # ----- Inicia estruturas de dados
 #Define tipo arqueiro
 class Arco(pygame.sprite.Sprite):
-    def __init__(self,img):
+    def __init__(self,img,todos,agulha, image_vacina):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = img
@@ -34,6 +36,9 @@ class Arco(pygame.sprite.Sprite):
         self.rect.centery = alturaw / 2
         self.rect.left = 0
         self.speedy = 0
+        self.todos = todos
+        self.agulha = agulha
+        self.image_vacina = image_vacina
 
     def update(self):
         self.rect.y += self.speedy
@@ -43,6 +48,12 @@ class Arco(pygame.sprite.Sprite):
         if self.rect.bottom > 650:
             self.rect.bottom = 650
 
+    # Cria a vacina
+    def vacinar(self):
+        vacina0 = Vacina(self.image_vacina,xlinha+20,self.rect.centery)
+        self.agulha.add(vacina0)
+        self.todos.add(vacina0)
+
 #Define linha
 class Linha(pygame.sprite.Sprite):
     def __init__(self,img):
@@ -50,7 +61,7 @@ class Linha(pygame.sprite.Sprite):
 
         self.image = img
         self.rect = self.image.get_rect()
-        self.rect.left = 250
+        self.rect.left = xlinha
         self.rect.top = -100
 
 #Define tipo corona
@@ -73,8 +84,23 @@ class Corona(pygame.sprite.Sprite):
             self.speedy = self.speedy * -1
         if self.rect.top >= 10:
             self.speedy = self.speedy * -1
-        if self.rect.left <= 250:
+        if self.rect.left <= xlinha:
             self.speedx = self.speedx * -1
+    
+#Define a vacina
+class Vacina(pygame.sprite.Sprite):
+    def __init__(self, img, left, centery):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centery = centery
+        self.rect.left = left
+        self.speedx = 25
+
+    def update(self):
+        self.rect.x  += self.speedx
+        if self.rect.left > larguraw:
+            self.kill()
 
 
 game = True
@@ -83,17 +109,18 @@ clock = pygame.time.Clock()
 FPS = 15
 
 #Cria o corona
-all_sprites = pygame.sprite.Group()
+todos = pygame.sprite.Group()
 corona = Corona(image_alvo)
-all_sprites.add(corona)
+todos.add(corona)
 coronaalvo = pygame.sprite.Group()
 coronaalvo.add(corona)
+agulha = pygame.sprite.Group()
 #Criando o player
-arco = Arco(image_arqueiro)
-all_sprites.add(arco)
+arco = Arco(image_arqueiro, todos, agulha, image_vacina)
+todos.add(arco)
 #Criando linha
 linha = Linha(image_linha)
-all_sprites.add(linha)
+todos.add(linha)
 
 # ===== Loop principal =====
 while game:
@@ -111,6 +138,8 @@ while game:
                 arco.speedy -= 12
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 arco.speedy += 12
+            if event.key == pygame.K_SPACE or event.key == pygame.K_q:
+                arco.vacinar()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_w:
                 arco.speedy += 12
@@ -118,18 +147,20 @@ while game:
                 arco.speedy -= 12
 
     # ----- Atualiza estado do jogo
-    all_sprites.update()
+    todos.update()
 
     # Corona bate na linha
     colisao = pygame.sprite.spritecollide(linha,coronaalvo,True) 
-    
+    if len(colisao)>0:
+        game = False
+
     # ----- Gera saídas
     window.fill((255, 255, 255))
     distancia_x = 0
     while distancia_x < 1501:
         window.blit(image_bloco2, (distancia_x, 650))
         distancia_x+=250
-    all_sprites.draw(window)
+    todos.draw(window)
     
     pygame.display.update()  # Mostra o novo frame para o jogador
 
